4bac39314a5f9a8b83b362287c6e2243c84fd403
Thomas Caruchet <thomas.caruchet@etu.univ-cotedazur.fr>
2022-02-25 10:08:26 +0100

    Programme 1 : contrats

---
+++ TD3/JCDecaux.Api/JCDAPIAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace JCDecaux.Api
+{
+    public static class JCDAPIAccess
+    {
+        public static readonly string Url = "https://api.jcdecaux.com/vls/v3/";
+        public static readonly string ApiKey = "47ff5058c14817b9c348ab9622c16f96fe818454";
+
+        public static string GetUrl(string parameters)
+        {
+            return Url + parameters + $"{(parameters.Contains("?") ? "&" : "?")}apiKey={ApiKey}";
+        }
+    }
+}
---
+++ TD3/JCDecauxTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JCDecauxTest.Contracts
+{
+    internal class Program
+    {
+        static readonly HttpClient Client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(JsonConvert.DeserializeObject<object>(await Client.GetStringAsync(JCDecaux.Api.JCDAPIAccess.GetUrl("contracts"))));
+            }
+            catch (HttpRequestException e)
+            {
+                if (e != null && (e is HttpRequestException || e is JsonSerializationException))
+                {
+                    Console.WriteLine("\nException Caught!");
+                    Console.WriteLine("Message :{0} ", e.Message);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
