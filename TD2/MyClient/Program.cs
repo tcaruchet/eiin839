fa0787b59d75d14a2e5ef887ca22848dbbfade2d
Thomas Caruchet <thomas.caruchet@etu.univ-cotedazur.fr>
2022-03-04 11:44:24 +0100

    Add MyClient for ex3 client

---
+++ TD2/MyClient/Program.cs
@@ -0,0 +1,25 @@
+﻿// NET 6.0 Notation
+// HTTP CLIENT
+
+using Microsoft.AspNetCore.WebUtilities;
+using System.Net.Mime;
+using System.Text;
+
+string BASE_URI = "http://localhost:8080";
+
+HttpClient client = new HttpClient();
+client.BaseAddress = new Uri(BASE_URI);
+
+//Method called & Parameters Dictionnary
+string requestUri = QueryHelpers.AddQueryString("Incr", new Dictionary<string, string>()
+{
+    { "value", "49" } //Change 49 
+});
+
+var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
+
+var response = await client.SendAsync(request).ConfigureAwait(false);
+response.EnsureSuccessStatusCode();
+
+var responseBody = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+Console.WriteLine(responseBody);
