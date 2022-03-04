55e858a9f48ce8ac730c6743eb821e050d3ce0d6
Thomas Caruchet <thomas.caruchet@etu.univ-cotedazur.fr>
2022-03-04 11:53:29 +0100

    Readd exectest + readme

---
+++ TD2/ExecTest/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+if (args.Length == 1)
+    Console.WriteLine(args[0]);
+else
+    Console.WriteLine("ExeTest <string parameter>");
